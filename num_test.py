df6b42faad47431f593f5843c56291dee050534d
srnagankeri <srnagankeri@gmail.com>
2021-03-18 08:49:47 -0700

    added python files

---
+++ num_test.py
@@ -0,0 +1,11 @@
+print("enter nums")
+num=list(map(int, input().split()))
+      
+print("numbers are", num)
+print("enter nums")
+x = input().split()
+print("numbers are ", format(x))
+
+print("enter numbers")
+x, y = [int(x) for x in input().split()]
+print(x, y)
